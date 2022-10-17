29e6c316ed541ffc0c5d55521ad83da9aa10f63b
PANKAJ RAJ <115636241+Pankaj179001@users.noreply.github.com>
2022-10-17 14:31:53 +0530

    Add files via upload

---
+++ asignment1.js
@@ -0,0 +1,69 @@
+//celcius to fahrenheight
+
+/*const chd =require('readline-sync')
+var c,f
+c=chd.questionInt('enter temprature in celcius')
+f=(9/5*c)+32
+console.log(`the temprature in fahrenheight is ${f}`)*/
+
+
+
+//bmi calc
+
+/*const chd=require('readline-sync')
+var weight,height,bmi
+weight =chd.question('enter your weight in kg')
+height =chd.question('enter your height in m')
+bmi=weight/(height*height)
+console.log(`the bmi is ${bmi}`)*/
+
+
+
+//kilometer to miles
+
+/*const chd =require('readline-sync')
+var km,mile
+km=chd.questionInt('enter the distance in kelometer')
+mile=km*0.6214//formula
+console.log(`the distance in mile is ${mile}`)*/
+
+
+
+//area of circle
+
+/* const chd=require('readline-sync')
+ var r,area
+ r=chd.question('enter radius of the circle')
+ area=3.14*r*r
+ console.log(`the area of the circle is ${area}`)*/
+
+
+
+ //swap two numbers
+
+ /*const abc=require('readline-sync')
+ var a,b,temp
+ a=abc.questionInt('enter first number \n')
+ b=abc.questionInt('enter second number \n')
+ temp=a
+ a=b
+ b=temp
+ console.log(`after swapping,the first no. is \n${a}`)
+ console.log(`after swapping,the second no. is \n${b}`)*/
+
+
+
+ //swap three numbers
+
+ /*const abc=require('readline-sync')
+ var first,second,third,temp
+ first=abc.questionInt('enter first no.\n')
+ second=abc.questionInt('enter second no.\n')
+ third=abc.questionInt('enter third no.\n')
+ temp=first
+ first=second
+ second=third
+ third=temp
+ console.log(`after swapping first no. is\n${first}`)
+ console.log(`after swapping second no. is\n${second}`)
+ console.log(`after swapping third no. is\n${third}`)*/
